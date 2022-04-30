1d0baeb90e088317ea689638c511c6706407211a
AZU-URE <202051069@iiitvadodara.ac.in>
2022-04-30 21:23:35 +0530

    added toolbar

---
+++ toolbar/app/src/main/java/com/example/toolbar/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_items,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.new_group:
+                Toast.makeText(this, "new group", Toast.LENGTH_SHORT).show();
+                break;
+                
+            case R.id.web:
+                Toast.makeText(this, "whatsapp web", Toast.LENGTH_SHORT).show();
+                    break;
+        }
+        return true;
+    }
+}
